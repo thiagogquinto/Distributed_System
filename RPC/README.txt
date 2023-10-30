df3c32cbad87b69c3e910189584a210071b01064
thiagogquinto <thiagogarianiq@gmail.com>
2023-10-30 10:51:34 -0300

    add code

---
+++ RPC/README.txt
@@ -0,0 +1,67 @@
+python3 -m grpc_tools.protoc -I../ --python_out=. --grpc_python_out=. ../movie.proto
+protoc --java_out=. --proto_path=../ ../movie.proto
+protoc -I. --java_out=. --grpc_out=. --plugin=protoc-gen-grpc=/usr/local/bin/protoc-gen-grpc-java exemplo.proto
+
+Como compilar:
+
+    Primeiramente, certifique-se que está no diretório javacode e execute o seguinte comando:
+    
+    javac -classpath .:protobuf-java-3.24.4.jar Client.java
+
+Como executar:
+
+Antes de executar o servidor é necessário instalar alguns módulos, para isso execute o seguinte comando:
+    pip install google protobuf bson pymongo
+
+Para executar o servidor é necessário estar no diretório pythoncode, execute o seguinte comando:
+    python3 server.py
+
+Para executar o cliente é necessário estar no diretório javacode, execute o seguinte comando:
+    java -classpath .:protobuf-java-3.24.4.jar Client
+
+
+Bibliotecas usadas:
+    Bibliotecas servidor:
+        import socket
+        import movie_pb2 
+        from bson import ObjectId
+        from pymongo import MongoClient
+        
+    Bibliotecas cliente:
+        import java.io.*;
+        import java.net.*;
+        import java.util.Arrays;
+
+Exemplos de uso:
+
+    Assim que o cliente se conectar com o servidor aparecerá as seguinte opções:
+
+        Escolha um comando: 
+        get_movies = 1 
+        get_movies_by_actor = 2 
+        get_movies_by_genre = 3 
+        add_movie = 4 
+        update_movie = 5 
+        delete_movie = 6 
+        exit = 7 
+
+    get_movies:
+        Digite 1
+
+    get_movies_by_actor:
+        Digite 2 e informe o nome do ator que deseja realizar a listagem
+
+    get_movies_by_genre:
+        Digite 3 e informe o gênero do filme que deseja realizar a listagem
+
+    add_movie:
+        Digite 4 e informe os dados conforme forem sendo pedidos
+
+    update_movie
+        Digite 5, informe o id do filme e informe os dados conforme forem sendo pedidos
+
+    delete_movie
+        Digite 6 e informe o id do filme que deseja deletar
+
+    exit
+        Digite 7
---
+++ RPC/pythoncode/server.py
@@ -0,0 +1,194 @@
+import socket
+import movie_pb2 
+import movie_pb2_grpc
+import grpc
+from concurrent import futures
+from bson import ObjectId
+from datetime import datetime
+from pymongo import MongoClient
+
+# Configurações do servidor
+HOST = '127.0.0.1'  # Endereço IP do servidor
+PORT = 12345         # Porta que o servidor vai escutar
+
+# Conecta-se ao MongoDB
+uri = "mongodb+srv://root:root@cluster0.ivcdldw.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+db = client.get_database('sample_mflix')
+movies_collection = db.movies
+
+# Cria um socket TCP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Liga o socket ao endereço e porta especificados
+server_socket.bind((HOST, PORT))
+
+# Habilita o socket para aceitar conexões
+server_socket.listen()
+
+print(f"Servidor escutando em {HOST}:{PORT}...")
+
+class MovieService(movie_pb2_grpc.MovieServiceServicer):
+
+    def handleRequest(self, request):
+        if request.operation == "get_movies":
+            return self.GetMovies(request)
+        elif request.operation == "get_movie_by_actor":
+            return self.GetMovieByActor(request)
+        elif request.operation == "get_movie_by_genre":
+            return self.GetMovieByGenre(request)
+        elif request.operation == "delete_movie":
+            return self.DeleteMovie(request)
+        elif request.operation == "update_movie":
+            return self.UpdateMovie(request)
+        elif request.operation == "add_movie":
+            return self.AddMovie(request)
+        else:
+            return movie_pb2.Empty()
+
+    def GetMovies(self, request, context):
+
+        movies = movies_collection.find({}, {"imdb":0, "tomatoes":0, "awards":0})
+        response = movie_pb2.Movies()
+
+        for movie in movies:
+            response.movies.append(movie_pb2.Movie(
+                id=str(movie['_id']),
+                plot=movie['plot'],
+                genres=movie['genres'],
+                runtime=movie['runtime'],
+                cast=movie['cast'],
+                num_mflix_comments=movie['num_mflix_comments'],
+                title=movie['title'],
+                fullplot=movie['fullplot'],
+                countries=movie['countries'],
+                released=movie['released'].strftime('%Y-%m-%d'),
+                directors=movie['directors'],
+                rated=movie['rated'],
+                lastupdated=movie['lastupdated'].strftime('%Y-%m-%d %H:%M:%S'),
+                year=movie['year'],
+                type=movie['type']
+            ))
+
+        return response
+    
+    def GetMovieByActor(self, request, context):
+        movies = movies_collection.find({"cast": request.actor}, {"imdb":0, "tomatoes":0, "awards":0})
+        response = movie_pb2.Movies()
+
+        for movie in movies:
+            response.movies.append(movie_pb2.Movie(
+                id=str(movie['_id']),
+                plot=movie['plot'],
+                genres=movie['genres'],
+                runtime=movie['runtime'],
+                cast=movie['cast'],
+                num_mflix_comments=movie['num_mflix_comments'],
+                title=movie['title'],
+                fullplot=movie['fullplot'],
+                countries=movie['countries'],
+                released=movie['released'].strftime('%Y-%m-%d'),
+                directors=movie['directors'],
+                rated=movie['rated'],
+                lastupdated=movie['lastupdated'].strftime('%Y-%m-%d %H:%M:%S'),
+                year=movie['year'],
+                type=movie['type']
+            ))
+
+        return response
+
+    def GetMovieByGenre(self, request, context):
+        movies = movies_collection.find({"genres": request.genre}, {"imdb":0, "tomatoes":0, "awards":0})
+        response = movie_pb2.Movies()
+
+        for movie in movies:
+            response.movies.append(movie_pb2.Movie(
+                id=str(movie['_id']),
+                plot=movie['plot'],
+                genres=movie['genres'],
+                runtime=movie['runtime'],
+                cast=movie['cast'],
+                num_mflix_comments=movie['num_mflix_comments'],
+                title=movie['title'],
+                fullplot=movie['fullplot'],
+                countries=movie['countries'],
+                released=movie['released'].strftime('%Y-%m-%d'),
+                directors=movie['directors'],
+                rated=movie['rated'],
+                lastupdated=movie['lastupdated'].strftime('%Y-%m-%d %H:%M:%S'),
+                year=movie['year'],
+                type=movie['type']
+            ))
+
+        return response
+    
+    def DeleteMovie(self, request, context):
+        result = movies_collection.delete_one({"_id": ObjectId(request.id)})
+
+        if result.deleted_count == 1:
+            return movie_pb2.DeleteMovieResponse(message="Filme deletado com sucesso!")
+        else:
+            return movie_pb2.DeleteMovieResponse(message="Filme não encontrado!") 
+
+    def UpdateMovie(self, request, context):
+
+        updated_movie = {
+            "plot": request.plot, 
+            "genres": list(request.genres),  
+            "runtime": request.runtime, 
+            "cast": list(request.cast), 
+            "num_mflix_comments": request.num_mflix_comments,  
+            "title": request.title, 
+            "fullplot": request.fullplot, 
+            "countries": list(request.countries), 
+            "released": datetime.strptime(request.released, '%Y-%m-%d'), 
+            "directors": list(request.directors), 
+            "rated": request.rated, 
+            "lastupdated": datetime.strptime(request.lastupdated, '%Y-%m-%d %H:%M:%S'), 
+            "year": request.year, 
+            "type": request.type, 
+        }
+
+        result = movies_collection.update_one({"_id": ObjectId(request.id)}, {"$set": updated_movie})
+
+        if result.modified_count == 1:
+            return movie_pb2.UpdateMovieResponse(response="Filme atualizado com sucesso!", movie=movie_pb2.Movie.MovieData(**updated_movie))
+        else:
+            return movie_pb2.UpdateMovieResponse(response="Filme não encontrado!", movie=movie_pb2.Movie().Empty())
+
+    def AddMovie(self, request, context):
+
+        movie_data = {
+            "plot": request.plot,
+            "genres": list(request.genres),
+            "runtime": request.runtime,
+            "cast": list(request.cast),
+            "num_mflix_comments": request.num_mflix_comments,
+            "title": request.title,
+            "fullplot": request.fullplot,
+            "countries": list(request.countries),
+            "released": request.released,
+            "directors": list(request.directors),
+            "rated": request.rated,
+            "lastupdated": request.lastupdated,
+            "year": request.year,
+            "type": request.type
+        }
+
+        result = movies_collection.insert_one(movie_data)
+
+        if result.inserted_id:
+            return movie_pb2.AddMovieResponse(response="Filme adicionado com sucesso! - ID: " + str(result.inserted_id), movie=movie_pb2.Movie.MovieData(**movie_data))
+        else:
+            return movie_pb2.AddMovieResponse(response="Erro ao adicionar filme!", movie=movie_pb2.Movie())
+        
+def main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    movie_pb2_grpc.add_MovieServiceServicer_to_server(MovieService(), server)
+    server.add_insecure_port('[::]:12345')
+    server.start()
+    print("Servidor escutando em 12345...")
+    server.wait_for_termination() 
+
+if __name__ == "__main__":
+    main()
