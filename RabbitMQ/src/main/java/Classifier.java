ccee145b00002728d4d2e5cf419093f99002e196
marcosquadross <marcosdquadros@gmail.com>
2023-11-07 15:27:32 -0300

    update classifier

--- RabbitMQ/src/main/java/Classifier.java
+++ RabbitMQ/src/main/java/Classifier.java
@@ -3,35 +3,34 @@ import java.util.*;
 import org.json.*;
 
 import java.nio.charset.StandardCharsets;
-import com.rabbitmq.client.Channel;
+
 import com.rabbitmq.client.*;
 
 public class Classifier {
 
     // Função para receber mensagens do RabbitMQ e retornar uma String com a mensagem
     public static String receiveMessage(String exchangeName, String routingKey) {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost"); // replace with your host if not localhost
-        final String[] message = new String[1];
-    
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            String queueName = channel.queueDeclare().getQueue();
-            channel.queueBind(queueName, exchangeName, routingKey);
-            // channel.queueDeclare(queueName, true, false, false, null);
-            DefaultConsumer consumer = new DefaultConsumer(channel) {
-                @Override
-                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                    message[0] = new String(body, StandardCharsets.UTF_8);
-                }
-            };
-            channel.basicConsume(queueName, true, consumer);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    
-        return message[0];
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost"); // replace with your host if not localhost
+    final StringBuilder content = new StringBuilder();
+
+    try (Connection connection = factory.newConnection();
+         Channel channel = connection.createChannel()) {
+        channel.queueBind("tweets_queue", exchangeName, routingKey);
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, StandardCharsets.UTF_8);
+                content.append(message);
+            }
+        };
+        channel.basicConsume("tweets_queue", true, consumer);
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+    System.out.println("\n [x] Received '" + content.toString() + "'\n");
+    return content.toString();
+}
 
     // Função para enviar mensagens para o RabbitMQ
     public static void sendMessage(String queueName, String message) {
@@ -71,28 +70,28 @@ public class Classifier {
 
     public static void main(String[] args) {
         Classifier classifier = new Classifier();
-        String msg = "    [{\r\n" + //
-                "        \"name\": \"aye_brenden\",\r\n" + //
-                "        \"description\": \"Averett Football #1 john 3:16 God,Family,Football\",\r\n" + //
-                "        \"text\": \"@kass_xo was it really that funny lol and how are you stranger lol\",\r\n" + //
-                "        \"topic\": \"football\"\r\n" + //
-                "    },\r\n" + //
-                "    {\r\n" + //
-                "        \"name\": \"user2\",\r\n" + //
-                "        \"description\": \"Another description\",\r\n" + //
-                "        \"text\": \"Another tweet text\",\r\n" + //
-                "        \"topic\": \"basketball\"\r\n" + //
-                "    },\r\n" + //
-                "    {\r\n" + //
-                "        \"name\": \"user3\",\r\n" + //
-                "        \"description\": \"Yet another description\",\r\n" + //
-                "        \"text\": \"Yet another tweet text\",\r\n" + //
-                "        \"topic\": \"basketball\"\r\n" + //
-                "    }]";
+        // String msg = "    [{\r\n" + //
+        //         "        \"name\": \"aye_brenden\",\r\n" + //
+        //         "        \"description\": \"Averett Football #1 john 3:16 God,Family,Football\",\r\n" + //
+        //         "        \"text\": \"@kass_xo was it really that funny lol and how are you stranger lol\",\r\n" + //
+        //         "        \"topic\": \"football\"\r\n" + //
+        //         "    },\r\n" + //
+        //         "    {\r\n" + //
+        //         "        \"name\": \"user2\",\r\n" + //
+        //         "        \"description\": \"Another description\",\r\n" + //
+        //         "        \"text\": \"Another tweet text\",\r\n" + //
+        //         "        \"topic\": \"basketball\"\r\n" + //
+        //         "    },\r\n" + //
+        //         "    {\r\n" + //
+        //         "        \"name\": \"user3\",\r\n" + //
+        //         "        \"description\": \"Yet another description\",\r\n" + //
+        //         "        \"text\": \"Yet another tweet text\",\r\n" + //
+        //         "        \"topic\": \"basketball\"\r\n" + //
+        //         "    }]";
         
-        sendMessage("topic", msg);
+        // sendMessage("topic", msg);
         
-        // classifier.teste();
+        classifier.teste();
     }
 }
 
